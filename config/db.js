7a5ecd5f63cb3f95e11342a81fd70704fcd80b4b
Rijvan07 <rijvanshaikh888@gmail.com>
2025-01-21 17:51:01 +0530

    add mongoURL in DB

--- config/db.js
+++ config/db.js
@@ -1,26 +1,6 @@
-// const { MongoClient } = require('mongodb');
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URL, {
-// 	dbName: process.env.DB_NAME,
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", function() {
-// 	console.log("Connected");
-// });
-// module.exports = MongoClient.connect(process.env.MONGODB_URL);
-
-///
-
 const { MongoClient } = require("mongodb");
 require('dotenv').config();
-const client = new MongoClient(process.env.MONGODB_URL, {
+const client = new MongoClient("mongodb+srv://arshshaikh6075:hYbxMnFWBsOZkf3v@cluster0.rem50.mongodb.net/CodeBase?retryWrites=true&w=majority&appName=Cluster0", {
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
 });
