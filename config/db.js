97d53963cea61720833eab03ba3a8791626416b0
Rijvan07 <rijvanshaikh888@gmail.com>
2025-01-21 17:42:57 +0530

    change db file

--- config/db.js
+++ config/db.js
@@ -21,8 +21,8 @@
 const { MongoClient } = require("mongodb");
 require('dotenv').config();
 const client = new MongoClient(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
 });
 
 client.connect()
