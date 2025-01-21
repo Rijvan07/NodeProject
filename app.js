d37a81bc4db0b832e371ec792b136e36688ccbe3
Rijvan07 <rijvanshaikh888@gmail.com>
2025-01-21 18:29:37 +0530

    new commit app.js file

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/', (req, res) => {
-    res.send('Hello Rushikesh React Developer...!');
+    res.send('Hello Rushikesh React Developer Lead...!');
 });
 
 // Start server
